cf6218a45d85c906162ef994f3bda12d633cb79c
sibelcaliskann <sibelcal4@gmail.com>
2025-04-27 17:01:17 +0300

    Add project files.

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistance.Context
+{
+    public class MovieContext : DbContext // Fix: Inherit from DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-SIBEL;initial Catalog=ApiMovieDb; integrated Security=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
